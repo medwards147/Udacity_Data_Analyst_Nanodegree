e426f4a760964a6283dddfa39672fe759c8c375d
medwards147 <medwards147@gmail.com>
2016-01-18 08:43:35 -0500

    first commit

---
+++ Project2_Subway/analysis.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 20:35:39 2015
+
+@author: MAX
+"""
+
+from ggplot import *
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import scipy.stats
+from sklearn.linear_model import SGDRegressor
+
+
+def entries_histogram(turnstile_weather):
+    '''
+    Before we perform any analysis, it might be useful to take a
+    look at the data we're hoping to analyze. More specifically, let's 
+    examine the hourly entries in our NYC subway data and determine what
+    distribution the data follows. This data is stored in a dataframe
+    called turnstile_weather under the ['ENTRIESn_hourly'] column.
+    
+    Let's plot two histograms on the same axes to show hourly
+    entries when raining vs. when not raining. Here's an example on how
+    to plot histograms with pandas and matplotlib:
+    turnstile_weather['column_to_graph'].hist()
+    
+    Your histogram may look similar to bar graph in the instructor notes below.
+    
+    You can read a bit about using matplotlib and pandas to plot histograms here:
+    http://pandas.pydata.org/pandas-docs/stable/visualization.html#histograms
+    
+    You can see the information contained within the turnstile weather data here:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+    '''
+
+    df = pandas.read_csv(turnstile_weather)
+    
+    plt.figure()
+    
+    df['ENTRIESn_hourly'][df['rain'] == 0].hist(alpha = 0.5) # historgram for hourly entries when it is raining
+    df['ENTRIESn_hourly'][df['rain'] == 1].hist(alpha = 0.5) # historgram for hourly entries when it is not raining
+    
+    plt.suptitle('Histogram of Entries Hourly')
+    plt.xlabel('Entries Hourly')
+    plt.ylabel('Frequency')
+    plt.legend(['No Rain', 'Rain'])
+    
+    return plt
+
+def mann_whitney_plus_means(turnstile_weather):
+    '''
+    This function should return:
+        1) the mean of entries with rain
+        2) the mean of entries without rain
+        3) the Mann-Whitney U-statistic and p-value comparing the number of entries
+           with rain and the number of entries without rain
+    '''
+    df = pandas.read_csv(turnstile_weather)
+    
+    with_rain_mean = np.mean(df['ENTRIESn_hourly'][df['rain'] == 1])
+    without_rain_mean = np.mean(df['ENTRIESn_hourly'][df['rain'] == 0])
+        
+    x = df['ENTRIESn_hourly'][df['rain'] == 1]
+    y = df['ENTRIESn_hourly'][df['rain'] == 0]
+    
+    U, p = scipy.stats.mannwhitneyu(x,y)
+    
+    return with_rain_mean, without_rain_mean, U, p
+
+
+def normalize_features(features):
+    ''' 
+    Returns the means and standard deviations of the given features, along with a normalized feature
+    matrix.
+    ''' 
+    means = np.mean(features, axis=0)
+    std_devs = np.std(features, axis=0)
+    normalized_features = (features - means) / std_devs
+    return means, std_devs, normalized_features
+
+def recover_params(means, std_devs, norm_intercept, norm_params):
+    ''' 
+    Recovers the weights for a linear model given parameters that were fitted using
+    normalized features. Takes the means and standard deviations of the original
+    features, along with the intercept and parameters computed using the normalized
+    features, and returns the intercept and parameters that correspond to the original
+    features.
+    ''' 
+    intercept = norm_intercept - np.sum(means * norm_params / std_devs)
+    params = norm_params / std_devs
+    return intercept, params
+
+def linear_regression(features, values):
+    """
+    Perform linear regression given a data set with an arbitrary number of features.
+    """
+    ### YOUR CODE GOES HERE ###
+    
+    model = SGDRegressor().fit(features,values)
+    #model.fit(features, values)
+    intercept = model.intercept_
+    params = model.coef_
+    
+    return intercept, params
+
+def predictions(dataframe):
+    '''
+    The NYC turnstile data is stored in a pandas dataframe called weather_turnstile.
+    Using the information stored in the dataframe, let's predict the ridership of
+    the NYC subway using linear regression with gradient descent.
+    
+    You can download the complete turnstile weather dataframe here:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
+    
+    Your prediction should have a R^2 value of 0.40 or better.
+    You need to experiment using various input features contained in the dataframe. 
+    We recommend that you don't use the EXITSn_hourly feature as an input to the 
+    linear model because we cannot use it as a predictor: we cannot use exits 
+    counts as a way to predict entry counts. 
+    
+    Note: Due to the memory and CPU limitation of our Amazon EC2 instance, we will
+    give you a random subset (~50%) of the data contained in 
+    turnstile_data_master_with_weather.csv. You are encouraged to experiment with 
+    this exercise on your own computer, locally.
+    
+    If you receive a "server has encountered an error" message, that means you are 
+    hitting the 30-second limit that's placed on running your program. Try using a
+    smaller number of features or fewer iterations.
+    '''
+    ################################ MODIFY THIS SECTION #####################################
+    # Select features. You should modify this section to try different features!             #
+    # We've selected rain, precipi, Hour, meantempi, and UNIT (as a dummy) to start you off. #
+    # See this page for more info about dummy variables:                                     #
+    # http://pandas.pydata.org/pandas-docs/stable/generated/pandas.get_dummies.html          #
+    ##########################################################################################
+    features = dataframe[['rain', 'precipi', 'Hour', 'meantempi']]
+    dummy_units = pandas.get_dummies(dataframe['UNIT'], prefix='unit')
+    features = features.join(dummy_units)
+    
+    # Values
+    values = dataframe['ENTRIESn_hourly']
+    
+    # Get numpy arrays
+    features_array = features.values
+    values_array = values.values
+    
+    means, std_devs, normalized_features_array = normalize_features(features_array)
+
+    # Perform gradient descent
+    norm_intercept, norm_params = linear_regression(normalized_features_array, values_array)
+    
+    intercept, params = recover_params(means, std_devs, norm_intercept, norm_params)
+    
+    predictions = intercept + np.dot(features_array, params)
+    # The following line would be equivalent:
+    # predictions = norm_intercept + np.dot(normalized_features_array, norm_params)
+    
+    return predictions    
+    
+if __name__ == '__main__':
+    filename = 'turnstile_data_master_with_weather.csv'
+    print entries_histogram(filename)
+    print mann_whitney_plus_means(filename)
+    test_results = mann_whitney_plus_means(filename)
+    print "this is", test_results[1]
+    
+   # linear_regression()
+
+    
+    
---
+++ Project2_Subway/rain_count_query.py
@@ -0,0 +1,252 @@
+import pandas
+import pandasql
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
+
+def num_rainy_days(filename):
+    '''
+    converts a CSV file into a dataframe and runs a SQL query, returning a
+    count of days when it rained.
+    '''
+    weather_data = pandas.read_csv(filename)
+    weather_data.rename(columns = lambda j: j.replace(' ', '_').lower(), 
+                        inplace=True)
+    
+    q = """
+    SELECT count(*)
+    FROM weather_data
+    WHERE rain=1;
+    """
+
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
+      
+def max_temp_aggregate_by_fog(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return two columns and
+    two rows - whether it was foggy or not (0 or 1) and the max
+    maxtempi for that fog value (i.e., the maximum max temperature
+    for both foggy and non-foggy days).  The dataframe will be 
+    titled 'weather_data'. You'll need to provide the SQL query.
+
+    '''
+    weather_data = pandas.read_csv(filename)
+    weather_data.rename(columns = lambda j: j.replace(' ', '_').lower(), 
+                        inplace=True)
+
+    q = """
+    SELECT fog, max(cast(maxtempi as integer))
+    FROM weather_data
+    GROUP BY fog;
+    """
+    
+    #Execute your SQL command against the pandas frame
+    foggy_days = pandasql.sqldf(q.lower(), locals())
+    return foggy_days
+
+def avg_weekend_temperature(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return one column and
+    one row - the average meantempi on days that are a Saturday
+    or Sunday (i.e., the the average mean temperature on weekends).
+    The dataframe will be titled 'weather_data' and you can access
+    the date in the dataframe via the 'date' column.
+    '''
+    weather_data = pandas.read_csv(filename)
+    weather_data.rename(columns = lambda j: j.replace(' ', '_').lower(), 
+                        inplace=True)
+    weather_data.rename(columns={'daten':'date'}, inplace=True) 
+
+    q = """
+    SELECT avg( cast (meantempi as integer))
+    FROM weather_data
+    WHERE cast (strftime('%w', date) as integer) = 0 or cast (strftime('%w', date) as integer) = 6;
+    """
+    
+    #Execute your SQL command against the pandas frame
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
+    
+def avg_min_temperature(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data. More specifically you want to find the average
+    minimum temperature (mintempi column of the weather dataframe) on 
+    rainy days where the minimum temperature is greater than 55 degrees.
+ 
+     '''
+    weather_data = pandas.read_csv(filename)
+    weather_data.rename(columns = lambda j: j.replace(' ', '_').lower(), 
+                        inplace=True)
+    
+    q = """
+    SELECT avg( cast (mintempi as integer))
+    FROM weather_data
+    WHERE rain = 1 and mintempi > 55
+    """
+    
+    #Execute your SQL command against the pandas frame
+    avg_min_temp_rainy = pandasql.sqldf(q.lower(), locals())
+    return avg_min_temp_rainy
+    
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    http://web.mta.info/developers/data/nyct/turnstile/turnstile_110507.txt
+    
+    As you can see, there are numerous data points included in each row of the
+    a MTA Subway turnstile text file. 
+
+    You want to write a function that will update each row in the text
+    file so there is only one entry per row. A few examples below:
+    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
+    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
+    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    
+    Write the updates to a different text file in the format of "updated_" + filename.
+    For example:
+        1) if you read in a text file called "turnstile_110521.txt"
+        2) you should write the updated data to "updated_turnstile_110521.txt"
+
+    The order of the fields should be preserved. Remember to read through the 
+    Instructor Notes below for more details on the task. 
+    
+    In addition, here is a CSV reader/writer introductory tutorial:
+    http://goo.gl/HBbvyy
+    
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file in the links below:
+    
+    Sample input file:
+    https://www.dropbox.com/s/mpin5zv4hgrx244/turnstile_110528.txt
+    Sample updated file:
+    https://www.dropbox.com/s/074xbgio4c39b7h/solution_turnstile_110528.txt
+    '''
+    
+    for name in filenames:
+        reader = csv.reader(open(name, 'rb'))
+        new_filename = "updated_"+name
+        writer = csv.writer(open(new_filename, 'wb'))
+        for row in reader:
+            first_3 = row[0:3]
+            for i in range(3, len(row), 5):
+               after_first_3 = row[i:i+5]
+               writer.writerow(first_3 + after_first_3)
+
+def create_master_turnstile_file(filenames, output_file):
+    '''
+    Write a function that takes the files in the list filenames, which all have the 
+    columns 'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn', and consolidates
+    them into one file located at output_file.  There should be ONE row with the column
+    headers, located at the top of the file. The input files do not have column header
+    rows of their own.
+    
+    For example, if file_1 has:
+    line 1 ...
+    line 2 ...
+    
+    and another file, file_2 has:
+    line 3 ...
+    line 4 ...
+    line 5 ...
+    
+    We need to combine file_1 and file_2 into a master_file like below:
+     'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn'
+    line 1 ...
+    line 2 ...
+    line 3 ...
+    line 4 ...
+    line 5 ...
+    '''
+    with open(output_file, 'w') as master_file:
+        column_headings = 'C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n'
+        master_file.write(column_headings)
+        for filename in filenames:
+            # your code here
+            input_file = open(filename, 'r')
+            for line in input_file:
+                if line.strip() == column_headings:
+                    pass
+                else:
+                    master_file.write(line)
+
+def filter_by_regular(filename):
+    '''
+    This function should read the csv file located at filename into a pandas dataframe,
+    and filter the dataframe to only rows where the 'DESCn' column has the value 'REGULAR'.
+    
+    For example, if the pandas dataframe is as follows:
+    ,C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    1,A002,R051,02-00-00,05-01-11,04:00:00,DOOR,3144335,1088159
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
+    3,A002,R051,02-00-00,05-01-11,12:00:00,DOOR,3144424,1088231
+    
+    The dataframe will look like below after filtering to only rows where DESCn column
+    has the value 'REGULAR':
+    0,A002,R051,02-00-00,05-01-11,00:00:00,REGULAR,3144312,1088151
+    2,A002,R051,02-00-00,05-01-11,08:00:00,REGULAR,3144353,1088177
+    '''
+    
+    data = pandas.read_csv(filename)
+    turnstile_data = data[data['DESCn'] == 'REGULAR']
+    return turnstile_data
+
+def get_hourly_entries(df):
+    '''
+    changes the cumulative entry numbers to a count of entries since the last reading
+    (i.e., entries since the last row in the dataframe).
+    '''
+    df['ENTRIESn_hourly'] = df['ENTRIESn'] - df['ENTRIESn'].shift(periods = 1)
+    df = df.fillna(1)
+    return df
+
+def get_hourly_exits(df):
+    '''
+    changes the cumulative exit numbers to a count of exits since the last reading
+    (i.e., exits since the last row in the dataframe).
+    '''
+    df['EXITSn_hourly'] = df['EXITSn'] - df['EXITSn'].shift(1)
+    df = df.fillna(0)
+    return df
+
+def time_to_hour(time):
+    '''
+    given an input variable time that represents time in the format of:
+    "00:00:00" (hour:minutes:seconds)
+    returns the hour as an integer
+    '''
+    datetime = pandas.to_datetime(time)
+    hour = datetime.hour
+    return hour
+
+def reformat_subway_dates(date):
+    '''
+    converts dates formatted in the format month-day-year to dates formatted
+    as year-month-day.
+    '''
+
+    d = datetime.strptime(date, '%m-%d-%y')
+    date_formatted = d.strftime('%Y-%m-%d')
+    return date_formatted
+
+
+if __name__ == '__main__':
+    print num_rainy_days('C:/Users/MAX/Documents/Udacity/Data Analyst Nanodegree/' +
+        'Project2_Subway/turnstile_data_master_with_weather.csv')
+    print max_temp_aggregate_by_fog('C:/Users/MAX/Documents/Udacity/Data Analyst Nanodegree/' +
+        'Project2_Subway/turnstile_data_master_with_weather.csv')
+    print avg_weekend_temperature('C:/Users/MAX/Documents/Udacity/Data Analyst Nanodegree/' + 
+        'Project2_Subway/turnstile_data_master_with_weather.csv')
+    print avg_min_temperature('C:/Users/MAX/Documents/Udacity/Data Analyst Nanodegree/' + 
+        'Project2_Subway/turnstile_data_master_with_weather.csv')
+    print "Data is now updated"
+    fix_turnstile_data(['turnstile_110528.txt'])
+    print open('updated_turnstile_110528.txt').read()
+    create_master_turnstile_file(['updated_turnstile_110528.txt'], 'master_turnstile.txt')
+    print filter_by_regular('master_turnstile.txt')
+    
